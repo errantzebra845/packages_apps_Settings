f58e633cf99932f348b54de257dc20606d9c7ea8
rc420head <dennislebhardt@yahoo.com>
2015-04-04 01:45:52 -0500

    Revert "Add:Network Signal"
    This reverts commit ccf1dcc824cb26d33cffcd87eb46f743ecc3b38a.
    
    Conflicts:
    	res/values/candy_arrays.xml
    	res/values/candy_attrs.xml

--- src/com/android/settings/fuelgauge/BatterySaverSettings.java
+++ src/com/android/settings/fuelgauge/BatterySaverSettings.java
@@ -149,7 +149,7 @@ public class BatterySaverSettings extends SettingsPreferenceFragment
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.add(0, MENU_RESET, 0, R.string.reset)
-                .setIcon(R.drawable.ic_settings_reset)
+                .setIcon(R.drawable.ic_menu_refresh_holo_dark)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
     }
 
